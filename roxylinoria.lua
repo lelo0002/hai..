1c1319abacf8d1fc5fa68f61388840fe74273af7
lelo0002 <backupce0819@gmail.com>
2026-05-07 10:58:03 -0700

    Update roxylinoria.lua

--- roxylinoria.lua
+++ roxylinoria.lua
@@ -1,4 +1,4 @@
--- gg 2323
+-- gg 2344
 local cloneref = (cloneref or clonereference or function(instance: any)
 	return instance
 end)
@@ -6041,8 +6041,8 @@ function BaseGroupboxFuncs:AddDependencyBox()
 
             LineGradient.Transparency = NumberSequence.new({
                 NumberSequenceKeypoint.new(0, HasPrev and 0 or 1),
-                NumberSequenceKeypoint.new(HasPrev and 0 or 0.05, 0),
-                NumberSequenceKeypoint.new(HasNext and 1 or 0.95, 0),
+                NumberSequenceKeypoint.new(HasPrev and 0 or 0.15, 0),
+                NumberSequenceKeypoint.new(HasNext and 1 or 0.85, 0),
                 NumberSequenceKeypoint.new(1, HasNext and 0 or 1)
             })
         end)
